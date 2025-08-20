51efebfd8c88f57a36e458fbe149ef644100390f
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-20 15:44:27 +0200

    Remove permissions group file

--- src/treeclick/groups/permissions.py
+++
@@ -1,5 +0,0 @@
-from treeclick.custom_group import CustomGroup
-from treeclick.groups.set_permissions import set_permissions
-
-permissions = CustomGroup(name="permissions", help="Manage user permissions.")
-permissions.add_command(set_permissions)
