f230eb7e09efca5d6fba35bb572cd0d4f6de3acf
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-20 15:44:27 +0200

    Remove old CLI test

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import click.testing
-from treeclick.cli import cli
-
-
-def test_info():
-    runner = click.testing.CliRunner()
-    result = runner.invoke(cli, ["info"])
-    assert result.exit_code == 0
-    assert "CLI Information" in result.output
